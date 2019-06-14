89385ed4f59691ebb4b5a506365952767d39d218
RunningHorse <2412546652@qq.com>
2019-06-14 19:26:44 +0800

    first commit

---
+++ Code/testMinTree.py
@@ -0,0 +1,20 @@
+import math
+import random
+from MinTree import *
+
+T = MinTree([1,4,2,5,3])
+#T.dump()
+print(T.getMin())
+#T.dump()
+print(T.getMin())
+exit(0)
+T.changeVal(4, 3)
+T.dump()
+T.getMin()
+T.dump()
+T.getMin()
+T.dump()
+T.changeVal(4, 10)
+T.dump()
+T.getMin()
+T.dump()
